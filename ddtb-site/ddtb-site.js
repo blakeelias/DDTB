a090ee535ab6316facfff44757d3996c71a38584
Blake Elias <blakeelias@gmail.com>
2015-08-26 19:42:07 -0400

    Display each queued message and associated Facebook user ID

--- ddtb-site/ddtb-site.js
+++ ddtb-site/ddtb-site.js
@@ -1,3 +1,6 @@
+Friends = new Mongo.Collection('friends');
+QueuedMessages = new Mongo.Collection('queuedmessages');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -5,6 +8,9 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    messages: function() {
+      return QueuedMessages.find({});
     }
   });
 
